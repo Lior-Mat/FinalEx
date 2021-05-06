ae6665d35c0b38a5e2ca116acd3b1d55274d8801
Daniell-Goldberg <83760306+Daniell-Goldberg@users.noreply.github.com>
2021-05-06 13:20:21 +0300

    Add files via upload

---
+++ hamburgerMenu.js
@@ -0,0 +1,17 @@
+      /* Set the width of the side navigation to 250px */
+        function openNav() {
+            
+            document.getElementById("mySidenav").style.width = "100%";
+            document.getElementById("effect").style.paddingRight = "250px";
+            document.getElementById("effect").style.transition = ".5s";
+
+        }
+
+        /* Set the width of the side navigation to 0 */
+        function closeNav() {
+            document.getElementById("mySidenav").style.width = "0";
+            document.getElementById("effect").style.transition = ".5s";
+            document.getElementById("effect").style.paddingRight = "0";
+
+        }
+
