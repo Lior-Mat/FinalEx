a313a50556d5fda3de87589d9b9b31a9b4ed7227
Daniel Goldberg <doudougoldberg@hotmail.com>
2021-05-06 22:53:54 +0300

    papuas

--- hamburgerMenu.js
+++ hamburgerMenu.js
@@ -1,10 +1,8 @@
       /* Set the width of the side navigation to 250px */
-        function openNav() {
-            
+        function openNav() {      
             document.getElementById("mySidenav").style.width = "100%";
             document.getElementById("effect").style.paddingRight = "250px";
             document.getElementById("effect").style.transition = ".5s";
-
         }
 
         /* Set the width of the side navigation to 0 */
@@ -14,4 +12,34 @@
             document.getElementById("effect").style.paddingRight = "0";
 
         }
+
+        let cart = [
+            {
+                name: "Pich",
+                id: 1,
+                size: 38,
+                imgURL: "https://fdajkngbdfknflgdfgn.com"
+            },
+            {
+                name: "Pich",
+                id: 2,
+                size: 38,
+                imgURL: "https://fdajkngbdfknflgdfgn.com"
+            },
+            {
+                name: "Pich",
+                id: 3,
+                size: 38,
+                imgURL: "https://fdajkngbdfknflgdfgn.com"
+            }
+            
+        ];
+
+        function onAddToCart({name: Srting, id: Number, size: Number, imgURL: Srting}) {
+            document.getElementById("mySidenav").style.width = "0";
+            document.getElementById("effect").style.transition = ".5s";
+            document.getElementById("effect").style.paddingRight = "0";
+        }
+
+        let x = document.querySelectorAll()
 
