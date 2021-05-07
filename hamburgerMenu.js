56a85e345c1629752cb331a712cc2cd043b3b21a
Daniel Goldberg <doudougoldberg@hotmail.com>
2021-05-07 15:16:46 +0300

    added functionalities for the cart

--- hamburgerMenu.js
+++ hamburgerMenu.js
@@ -13,13 +13,56 @@
 
         }
 
-        // let cart = [
+        // Hamburger Menu Code
+//////////////////////////////////////////////////////////////////////////////// 
+        
+        // My Fake DataBase
+        let items = [
+            {
+                // id: "1",
+                name:"Nike Air",
+                description:"blalbllblbalab", 
+                color:"red", 
+                size:"42", 
+                price:"150"
+            },
+
+            {
+                // id: "2",
+                name:"Nike Air2",
+                description:"blalbllblbalab", 
+                color:"red", 
+                size:"42", 
+                price:"150"
+            },
+
+            {
+                // id: "3",
+                name:"Nike Air3",
+                description:"blalbllblbalab", 
+                color:"red", 
+                size:"42", 
+                price:"150"
+            }
             
-        // ];
+        ];
+////////////////////////////////////////////////////////////
 
-        // function onAddToCart(item) {
-        //     cart.push(item);
-        // }
+        let cart = []; // Cart Array
+        let addedObjects = 0; // Cart Count
 
-        // let x = document.querySelectorAll()
+        
+        
+        // Adding items in the cart Array & inserting the count next to the cart icon.
+        function onAddToCart(item) {
+            addedObjects++;
+            cart.push(item);
+            console.log(cart);
+
+            if(addedObjects >= 9){
+                cartCountID.innerHTML = '<i class="fa fa-shopping-cart" aria-hidden="true"></i>' + '' + '  9+';
+            }
+            else
+            cartCountID.innerHTML = '<i class="fa fa-shopping-cart" aria-hidden="true"></i>' + '  ' + addedObjects;
+        }
 
