b7d1bd7dc71b8fc80b13559151fa96604796a121
Daniel Goldberg <doudougoldberg@hotmail.com>
2021-05-08 19:37:29 +0300

    Fixed the object pushing issue about getting the color

--- hamburgerMenu.js
+++ hamburgerMenu.js
@@ -14,7 +14,35 @@
         }
 
         // Hamburger Menu Code
+
+
+
 //////////////////////////////////////////////////////////////////////////////// 
+
+
+
+
+// Opening & Closing cart functions
+function openCart() {      
+    // myCartDisplay.innerHTML += cart.forEach(item => {
+    // `<div>Product Name: ${item.name}
+    //  Product Color: ${item.color}
+    //  Product Size: ${item.size}
+    //  Product Price: ${item.price}</div>
+    //  -----------------------------` 
+    // }); WE HAVE A PROBLEM TO INJECT AND DISPLAY OUR CART DATA ARRAY IN THE CART
+}
+
+/* Set the width of the side navigation to 0 */
+function closeCart() {
+}
+
+// forEach function
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////
         
         // My Fake DataBase
         let items = [
@@ -46,11 +74,27 @@
             }
             
         ];
+
 ////////////////////////////////////////////////////////////
+
+
 
         let cart = []; // Cart Array
         let addedObjects = 0; // Cart Count
 
+///////////////// GET AND SET METHODS FOR THE COLOR 
+        let saveColor = ''; // Saving my current clicked color
+
+        function onClickSetColor(id){
+            console.log(id);
+            saveColor = id; // Saving the color from the get method into the general variable saveColor
+        }
+
+        function onClickGetColor(){
+            return saveColor; // Sending back the saved color from the first function
+        }
+
+///////////////////////////////////////////////////////////////////////////////////////////////
         
         
         // Adding items in the cart Array & inserting the count next to the cart icon.
@@ -60,9 +104,9 @@
             console.log(cart);
 
             if(addedObjects >= 9){
-                cartCountID.innerHTML = '<i class="fa fa-shopping-cart" aria-hidden="true"></i>' + '' + '  9+';
+                cartCountID.innerHTML =  '  9+';
             }
             else
-            cartCountID.innerHTML = '<i class="fa fa-shopping-cart" aria-hidden="true"></i>' + '  ' + addedObjects;
+            cartCountID.innerHTML =  ' ' + addedObjects;
         }
 
